879f5d396b8db8fc441bba8e89989b334deeda20
Sanjeev <sanjeev.singh@traveloka.com>
2020-04-10 00:17:03 +0530

    Initial Version

---
+++ src/main/java/com/tatacliq/interview/tatacliqroot/TatacliqRootApplication.java
@@ -0,0 +1,13 @@
+package com.tatacliq.interview.tatacliqroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatacliqRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TatacliqRootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tatacliq/interview/tatacliqroot/TatacliqRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatacliq.interview.tatacliqroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatacliqRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
